7db6c4ae2df9d044e8f846d60f37bc147cedcd38
noppoman <yuki@miketokyo.com>
2016-06-21 23:17:28 +0900

    Update deps

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "Render",
 	dependencies: [
-      .Package(url: "https://github.com/slimane-swift/Renderable.git", majorVersion: 0, minor: 3),
-      .Package(url: "https://github.com/slimane-swift/AsyncResponderConvertible.git", majorVersion: 0, minor: 3),
+      .Package(url: "https://github.com/slimane-swift/Renderable.git", majorVersion: 0, minor: 4),
+      .Package(url: "https://github.com/slimane-swift/AsyncResponderConvertible.git", majorVersion: 0, minor: 4),
   ]
 )
